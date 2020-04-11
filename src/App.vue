f7c8be3c156d1def31581e74d3901c160b3f18c1
Gaboa <dmitry.zhabko@gmail.com>
2020-04-11 19:46:06 +0300

    Add form and data fields

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,16 @@
   <div id="app">
     <h3>Firebase Data</h3>
     {{ firebaseData }}
-  
+
+    <form>
+
+      <input type="text"   name="name">
+      <input type="number" name="age">
+      <input type="email"  name="email">
+
+      <button type="submit">SUBMIT</button>
+
+    </form>
   </div>
 </template>
 
@@ -11,6 +20,12 @@ import { db } from './firebase'
 const docPath = 'contacts/dima'
 
 export default {
+  data() {
+    return {
+      firebaseData: null,
+      formData: {}
+    }
+  },
   firestore() {
     return {
       firebaseData: db.doc(docPath)

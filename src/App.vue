48ae47a11762d1a124fb282cdf141e0eefdeed70
Gaboa <dmitry.zhabko@gmail.com>
2020-04-11 19:45:51 +0300

    Connect to the firestore doc

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
 <template>
   <div id="app">
+    <h3>Firebase Data</h3>
+    {{ firebaseData }}
   
   </div>
 </template>
 
 <script>
+import { db } from './firebase'
+const docPath = 'contacts/dima'
 
 export default {
-
+  firestore() {
+    return {
+      firebaseData: db.doc(docPath)
+    }
+  }
 }
 </script>
 
